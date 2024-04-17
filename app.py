00303d6de6cd5a319065fc0e2b72e648eef3b9d7
20syl <20syl.lam78@gmail.com>
2024-04-17 20:03:36 +0200

    Update app.py
    Simplified variables,
    Add options to /personal endpoint

--- app.py
+++ app.py
@@ -84,19 +84,19 @@ def generate_personal():
     tel = ['123-456-7890', '06 78 90 12 34', '7911 123456', '678 901 234', '163 555 1584']
     pays = ['US', 'FR', 'UK', 'ES', 'DE']
     job = ['Writer', 'Artist', 'Musician', 'Explorer', 'Scientist', 'Engineer', 'Athlete', 'Doctor', 'Teacher', 'Lawyer', 'Entrepreneur', 'Actor', 'Dancer', 'Photographer', 'Architect', 'Pilot', 'Designer', 'Journalist', 'Veterinarian']
-    card = ' '.join([str(random.randint(1000, 9999)) for _ in range(4)])
+    carte = ' '.join([str(random.randint(1000, 9999)) for _ in range(4)])
     cvc = random.randint(100, 999)
-    expiration_year = str(random.randint(datetime.now().year, datetime.now().year + 3))[-2:]
-    expiration_month = random.randint(1, 12)
+    annee = str(random.randint(datetime.now().year, datetime.now().year + 3))[-2:]
+    mois = random.randint(1, 12)
     
     i = random.randint(0, len(noms) - 1)
     
-    if expiration_month < 10:
-        expiration_date = f"0{expiration_month}/{expiration_year}"
+    if mois < 10:
+        date = f"0{mois}/{annee}"
     else:
-        expiration_date = f"{expiration_month}/{expiration_year}"
+        date = f"{mois}/{annee}"
         
-    return jsonify({'nom': noms[i], 'email': emails[i], 'tel': tel[i], 'pays': pays[i], 'profession': random.choice(job), 'card_numbers': card, 'cvc': cvc, 'expiration': expiration_date})
+    return jsonify({'nom': noms[i], 'email': emails[i], 'tel': tel[i], 'pays': pays[i], 'profession': random.choice(job), 'carte': carte, 'cvc': cvc, 'expiration': date})
 
 @app.errorhandler(404)
 def page_not_found(e):
