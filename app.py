96fe1ec43cc7923d97189381d79f38d0036810ab
20syl <20syl.lam78@gmail.com>
2024-05-04 01:58:07 +0200

    Update app.py
    Add /versions endpoint
    Add requests, os import & dotenv
    Edit function names
    Edit errors (more specific)

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, send_from_directory, send_file, request, jsonify
 from datetime import datetime
 from PIL import Image
-import random, string, qrcode, io
+from dotenv import load_dotenv
+import random, requests, string, qrcode, os, io
 
 ############################## PRINCIPAL ##############################
 
-# Création de l'app Flask
+# Création de l'app Flask & Charger le dotenv
 app = Flask(__name__, template_folder='src', static_folder='src')
+load_dotenv()
 
 # Page d'accueil
 @app.route('/')
@@ -39,39 +41,41 @@ def redirect_page(lang, endpoint):
     
     # Fonction exécutée en fonction du service demandé
     if endpoint == 'color':
-        return generate_color(lang)
+        return color(lang)
     elif endpoint == 'domain':
-        return generate_domain(lang)
+        return domain(lang)
     elif endpoint == 'lorem':
-        return generate_lorem(lang)
+        return lorem(lang)
     elif endpoint == 'personal':
-        return generate_personal(lang)
+        return personal(lang)
     elif endpoint == 'qrcode':
-        return generate_qrcode(lang)
+        return qrcode(lang)
     elif endpoint == 'token':
-        return generate_token(lang)
+        return token(lang)
+    elif endpoint == 'versions':
+        return versions(lang)
     elif endpoint == 'username':
-        return generate_username(lang)
+        return username(lang)
     else:
         return jsonify({'error': 'Endpoint not found'})
 
 ####################### FONCTIONS DES ENDPOINTS #########################
 
 # Génération de couleurs
-def generate_color(lang):
+def color(lang):
     r, g, b = [random.randint(0, 255) for _ in range(3)]
     return jsonify({'hex': '#{0:02x}{1:02x}{2:02x}'.format(r, g, b), 'rgb': f'rgb({r}, {g}, {b})'})
 
 # Génération de domaine
-def generate_domain(lang):
+def domain(lang):
     noms = ['example', 'site', 'test', 'demo']
     extensions = ['.com', '.fr', '.eu', '.dev', '.net', '.org', '.io']
     domain = random.choice(noms) + random.choice(extensions)
     return jsonify({'domain': domain, 'random_name': random.choice(noms), 'random_tld': random.choice(extensions)})
 
 # Génération de texte Lorem
-def generate_lorem(lang):
-    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra, dui non cursus accumsan, lectus leo euismod augue, a gravida odio sapien quis elit. Vivamus vehicula nisi vitae lacinia elementum. Ut id velit quis velit feugiat elementum. Vivamus vulputate, nisl ac placerat laoreet, ex nisl suscipit ligula, volutpat dapibus elit neque non orci. Pellentesque quis lacus efficitur, egestas nisl vitae, tincidunt justo. In gravida enim et egestas dapibus. Cras tempus libero ut sapien efficitur egestas. Sed commodo volutpat suscipit. Proin vulputate ante nec lacus consequat ullamcorper. Pellentesque facilisis massa quam, in auctor neque imperdiet sit amet. Maecenas sagittis aliquam eleifend. Sed dapibus, urna ut molestie fringilla, orci lorem aliquet lacus, nec interdum tortor lacus eu mauris. Curabitur vitae faucibus mi. Duis eget tempus justo. Curabitur placerat nisl eu sem dictum, eget placerat lectus vehicula. Fusce sit amet ipsum in libero aliquam mattis sit amet quis mi. Suspendisse sollicitudin consequat diam sit amet luctus. Nunc dapibus dolor vel sagittis faucibus. Donec et lorem ante. Praesent bibendum ultricies dui, at posuere leo vulputate eu. Integer gravida ex vitae erat accumsan egestas. Sed id tellus nec felis volutpat congue. Ut eget suscipit tellus, et tempus massa. Aliquam id semper diam, feugiat congue arcu. Nunc pulvinar et nisi eu elementum. Morbi condimentum sapien at sapien gravida, vitae lacinia orci condimentum. Suspendisse scelerisque urna quis euismod mattis. Pellentesque pulvinar pretium massa at varius. In pulvinar, velit ac mattis scelerisque, mauris nibh aliquam ipsum, sed aliquam tellus erat quis lectus. Nunc semper enim a felis ultrices convallis. Etiam velit magna, porta nec dui sit amet, venenatis gravida ipsum. In vitae magna et orci consequat cursus id vitae ex. Nulla pharetra massa vel felis malesuada porttitor. Etiam convallis tellus a sodales ullamcorper. Sed hendrerit sollicitudin erat, sit amet sodales magna dignissim nec. Curabitur ut enim eget dolor tincidunt bibendum. Fusce fermentum quam et finibus laoreet. Aliquam vel sem vitae turpis sollicitudin tempus. Nam rutrum risus ultricies quam tempor mattis. Pellentesque at dui felis. Cras maximus malesuada metus vitae iaculis. Aliquam erat volutpat. Nunc facilisis pretium tellus, ut tincidunt lectus eleifend a. In vel lacinia est. Integer tristique non metus pulvinar vestibulum. Fusce sit amet velit vel libero viverra semper et vel sem. In justo libero, semper a mi varius, aliquet commodo eros. Pellentesque porttitor tellus sit amet est varius luctus. Mauris placerat porta eros. Aliquam et mi id justo auctor semper ac vitae tellus. Donec auctor porttitor enim, at ornare ante ornare vel. Pellentesque sagittis ligula vitae libero vestibulum eleifend. Duis sit amet ullamcorper leo. Integer id dui velit. Quisque posuere nulla ligula, at consectetur metus finibus quis. Vestibulum sit amet neque iaculis, malesuada ipsum eget, feugiat nibh. Donec id ultricies enim, tincidunt volutpat arcu. Etiam vitae metus in mauris semper tempus. In dapibus neque at lorem pulvinar congue. Etiam ultrices tristique leo id interdum"
+def lorem(lang):
+    text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra, dui non cursus accumsan, lectus leo euismod augue, a gravida odio sapien quis elit. Vivamus vehicula nisi vitae lacinia elementum. Ut id velit quis velit feugiat elementum. Vivamus vulputate, nisl ac placerat laoreet, ex nisl suscipit ligula, volutpat dapibus elit neque non orci. Pellentesque quis lacus efficitur, egestas nisl vitae, tincidunt justo. In gravida enim et egestas dapibus. Cras tempus libero ut sapien efficitur egestas. Sed commodo volutpat suscipit. Proin vulputate ante nec lacus consequat ullamcorper. Pellentesque facilisis massa quam, in auctor neque imperdiet sit amet. Maecenas sagittis aliquam eleifend. Sed dapibus, urna ut molestie fringilla, orci lorem aliquet lacus, nec interdum tortor lacus eu mauris. Curabitur vitae faucibus mi. Duis eget tempus justo. Curabitur placerat nisl eu sem dictum, eget placerat lectus vehicula. Fusce sit amet ipsum in libero aliquam mattis sit amet quis mi. Suspendisse sollicitudin consequat diam sit amet luctus. Nunc dapibus dolor vel sagittis faucibus. Donec et lorem ante. Praesent bibendum ultricies dui, at posuere leo vulputate eu. Integer gravida ex vitae erat accumsan egestas. Sed id tellus nec felis volutpat congue. Ut eget suscipit tellus, et tempus massa. Aliquam id semper diam, feugiat congue arcu. Nunc pulvinar et nisi eu elementum. Morbi condimentum sapien at sapien gravida, vitae lacinia orci condimentum. Suspendisse scelerisque urna quis euismod mattis. Pellentesque pulvinar pretium massa at varius. In pulvinar, velit ac mattis scelerisque, mauris nibh aliquam ipsum, sed aliquam tellus erat quis lectus. Nunc semper enim a felis ultrices convallis. Etiam velit magna, porta nec dui sit amet, venenatis gravida ipsum. In vitae magna et orci consequat cursus id vitae ex. Nulla pharetra massa vel felis malesuada porttitor. Etiam convallis tellus a sodales ullamcorper. Sed hendrerit sollicitudin erat, sit amet sodales magna dignissim nec. Curabitur ut enim eget dolor tincidunt bibendum. Fusce fermentum quam et finibus laoreet. Aliquam vel sem vitae turpis sollicitudin tempus. Nam rutrum risus ultricies quam tempor mattis. Pellentesque at dui felis. Cras maximus malesuada metus vitae iaculis. Aliquam erat volutpat. Nunc facilisis pretium tellus, ut tincidunt lectus eleifend a. In vel lacinia est. Integer tristique non metus pulvinar vestibulum. Fusce sit amet velit vel libero viverra semper et vel sem. In justo libero, semper a mi varius, aliquet commodo eros. Pellentesque porttitor tellus sit amet est varius luctus. Mauris placerat porta eros. Aliquam et mi id justo auctor semper ac vitae tellus. Donec auctor porttitor enim, at ornare ante ornare vel. Pellentesque sagittis ligula vitae libero vestibulum eleifend. Duis sit amet ullamcorper leo. Integer id dui velit. Quisque posuere nulla ligula, at consectetur metus finibus quis. Vestibulum sit amet neque iaculis, malesuada ipsum eget, feugiat nibh. Donec id ultricies enim, tincidunt volutpat arcu. Etiam vitae metus in mauris semper tempus. In dapibus neque at lorem pulvinar congue. Etiam ultrices tristique leo id interdum'
     argument = request.args.get('len', '')
     if not argument.isdigit() or int(argument) < 1:
         argument = 1
@@ -81,7 +85,7 @@ def generate_lorem(lang):
     return jsonify({'text': output + '.'})
 
 # Génération d'informations personnelles
-def generate_personal(lang):
+def personal(lang):
     noms = ['John Doe', 'Jane Martin', 'Michael Johnson', 'Emily Davis', 'Alexis Barbos']
     emails = ['john@example.com', 'jane@example.com', 'michael@example.com', 'emily@example.com', 'alexis@example.com']
     tel = ['123-456-7890', '06 78 90 12 34', '7911 123456', '678 901 234', '163 555 1584']
@@ -95,22 +99,22 @@ def generate_personal(lang):
     i = random.randint(0, len(noms) - 1)
     
     if mois < 10:
-        date = f"0{mois}/{annee}"
+        date = f'0{mois}/{annee}'
     else:
-        date = f"{mois}/{annee}"
+        date = f'{mois}/{annee}'
         
     return jsonify({'name': noms[i], 'email': emails[i], 'tel': tel[i], 'localisation': pays[i], 'job': random.choice(job), 'card': carte, 'cvc': cvc, 'expiration': date})
 
 # Génération de QR code
-def generate_qrcode(lang):
+def qrcode(lang):
     url = request.args.get('url', '')
     if not url:
         if lang == 'en':
-            return jsonify({'error': 'Please provide a valid argument (?url={URL})'})
+            return jsonify({'error': 'Please provide a valid url (?url={URL})'})
         elif lang == 'fr':
-            return jsonify({'erreur': 'Veuillez fournir un argument valide (?url={URL})'})
+            return jsonify({'erreur': 'Veuillez fournir une URL valide (?url={URL})'})
         else:
-            return jsonify({'erreur': 'Veuillez fournir un argument valide (?url={URL})'})
+            return jsonify({'erreur': 'Veuillez fournir une URL valide (?url={URL})'})
 
     qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=18, border=3)
     qr.add_data(url)
@@ -124,7 +128,7 @@ def generate_qrcode(lang):
     return send_file(img_bytes, mimetype='image/png')
 
 # Génération de token
-def generate_token(lang):
+def token(lang):
     longueur = request.args.get('len', '')
     tokenType = request.args.get('type', '')
 
@@ -147,8 +151,58 @@ def generate_token(lang):
     
     return jsonify({'key': token})
 
+# Affichage des versions de mes projets
+def versions(lang):
+    source = request.args.get('source', '')
+    token = request.args.get('token', '')
+
+    if source == 'api':
+        portfolio = '1.4.0'
+        api = '1.3.0'
+        database = '1.0.1'
+        doc_coopbot = '1.0.0 alpha 0.8'
+        coop_status = '1.1.0'
+        coop_api = '1.3.0'
+        nitrogen = '1.1.0'
+
+    elif source == 'github' and token != os.getenv('TOKEN'):
+        if lang == 'en':
+            return jsonify({'error': 'Please provide a valid token (&token={TOKEN})'})
+        elif lang == 'fr':
+            return jsonify({'erreur': 'Veuillez fournir un token valide (&token={TOKEN})'})
+        else:
+            return jsonify({'erreur': 'Veuillez fournir un token valide (&token={TOKEN})'})
+
+    elif source != 'github' and token == os.getenv('TOKEN'):
+        if lang == 'en':
+            return jsonify({'error': 'Please provide a valid source (?source={SOURCE}&token={TOKEN})'})
+        elif lang == 'fr':
+            return jsonify({'erreur': 'Veuillez fournir une source valide (?source={SOURCE}&token={TOKEN})'})
+        else:
+            return jsonify({'erreur': 'Veuillez fournir une source valide (?source={SOURCE}&token={TOKEN})'})
+
+    elif source == 'github' and token == os.getenv("TOKEN"):
+        portfolio = requests.get('https://api.github.com/repos/20syldev/portfolio/releases').json()[0]['tag_name'].replace('-', ' ')
+        api = requests.get('https://api.github.com/repos/20syldev/api/releases').json()[0]['tag_name'].replace('-', ' ')
+        database = requests.get('https://api.github.com/repos/20syldev/database/releases').json()[0]['tag_name'].replace('-', ' ')
+        doc_coopbot = requests.get('https://api.github.com/repos/20syldev/doc-coopbot/releases').json()[0]['tag_name'].replace('-', ' ')
+        coop_status = requests.get('https://api.github.com/repos/20syldev/coop-status/releases').json()[0]['tag_name'].replace('-', ' ')
+        coop_api = requests.get('https://api.github.com/repos/20syldev/coop-api/releases').json()[0]['tag_name'].replace('-', ' ')
+        nitrogen = requests.get('https://api.github.com/repos/20syldev/nitrogen/releases').json()[0]['tag_name'].replace('-', ' ')
+
+    else:
+        portfolio = '1.4.0'
+        api = '1.3.0'
+        database = '1.0.1'
+        doc_coopbot = '1.0.0 alpha 0.8'
+        coop_status = '1.1.0'
+        coop_api = '1.3.0'
+        nitrogen = '1.1.0'
+
+    return jsonify({'portfolio': portfolio, 'api': api, 'database': database, 'doc_coopbot': doc_coopbot, 'coop_status': coop_status, 'coop_api': coop_api, 'nitrogen': nitrogen})
+
 # Génération de nom d'utilisateur
-def generate_username(lang):
+def username(lang):
     adj = ['Happy', 'Silly', 'Clever', 'Creative', 'Brave', 'Gentle', 'Kind', 'Funny', 'Wise', 'Charming', 'Sincere', 'Resourceful', 'Patient', 'Energetic', 'Adventurous', 'Ambitious', 'Courageous', 'Courteous', 'Determined']
     ani = ['Cat', 'Dog', 'Tiger', 'Elephant', 'Monkey', 'Penguin', 'Dolphin', 'Lion', 'Bear', 'Fox', 'Owl', 'Giraffe', 'Zebra', 'Koala', 'Rabbit', 'Squirrel', 'Panda', 'Horse', 'Wolf', 'Eagle']
     job = ['Writer', 'Artist', 'Musician', 'Explorer', 'Scientist', 'Engineer', 'Athlete', 'Chef', 'Doctor', 'Teacher', 'Lawyer', 'Entrepreneur', 'Actor', 'Dancer', 'Photographer', 'Architect', 'Pilot', 'Designer', 'Journalist', 'Veterinarian']
