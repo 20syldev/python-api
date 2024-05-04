75800d33c03ce3bb4f53c62e3e365e8642b27eb6
20syl <20syl.lam78@gmail.com>
2024-05-04 19:48:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ def token(lang):
 
 # Mettre à jour Firebase
 def update_database():
-    firebaseConfig = credentials.Certificate(os.getenv('KEYS'))
+    firebaseConfig = credentials.Certificate("/etc/secrets/dataKeys.json")
     firebase_admin.initialize_app(firebaseConfig)
     db = firestore.client()
     data = {
